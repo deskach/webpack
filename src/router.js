98e5e88cf25a62549f0841cb9195825dc160095b
deskach <dzianis.kachanau@gmail.com>
2017-10-07 17:31:09 +1100

    Introduced js-code split based on react routes

--- src/router.js
+++ src/router.js
@@ -1,21 +1,58 @@
 import React from 'react';
-import {hashHistory, IndexRoute, Route, Router} from 'react-router';
+import {hashHistory, Router} from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+// import ArtistDetail from './components/artists/ArtistDetail';
+// import ArtistCreate from './components/artists/ArtistCreate';
+// import ArtistEdit from './components/artists/ArtistEdit';
+
+
+/* Due to webpack's limitation it is hard to refactor this into a factory method.
+*  As webpack only handles static strings and will not generate separate bundles
+*  for each of the routes if we try to do something like System.import(fname) */
+const componentRoutes = {
+    component: Home,
+    path: '/',
+    indexRoute: {component: ArtistMain},
+    childRoutes: [
+        {
+            path: 'artists/new',
+            getComponent(location, cb) {
+                System.import('./components/artists/ArtistCreate')
+                    .then(module => cb(null, module.default))
+                // ^needed to make webpack split this into a separate js-file
+            }
+        },
+        {
+            path: 'artists/:id',
+            getComponent(location, cb) {
+                System.import('./components/artists/ArtistDetail')
+                    .then(module => cb(null, module.default))
+                // ^needed to make webpack split this into a separate js-file
+            }
+        },
+        {
+            path: 'artists/:id/edit',
+            getComponent(location, cb) {
+                System.import('./components/artists/ArtistEdit')
+                    .then(module => cb(null, module.default))
+                // ^needed to make webpack split this into a separate js-file
+            }
+        },
+    ]
+};
 
 const Routes = () => {
     return (
-        <Router history={hashHistory}>
-            <Route path="/" component={Home}>
-                <IndexRoute component={ArtistMain}/>
-                <Route path="artists/new" component={ArtistCreate}/>
-                <Route path="artists/:id" component={ArtistDetail}/>
-                <Route path="artists/:id/edit" component={ArtistEdit}/>
-            </Route>
+        <Router history={hashHistory} routes={componentRoutes}>
+            {/*<Router history={hashHistory}>*/}
+            {/*<Route path="/" component={Home}>*/}
+            {/*<IndexRoute component={ArtistMain}/>*/}
+            {/*<Route path="artists/new" component={ArtistCreate}/>*/}
+            {/*<Route path="artists/:id" component={ArtistDetail}/>*/}
+            {/*<Route path="artists/:id/edit" component={ArtistEdit}/>*/}
+            {/*</Route>*/}
         </Router>
     );
 };
